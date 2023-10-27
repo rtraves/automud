a9c21f20798c3dd9452cf3f267353dae9451dcaf
jgillway <jgillway@gmail.com>
2023-10-26 20:52:51 -0400

    removed console.log

--- src/npc.ts
+++ src/npc.ts
@@ -144,7 +144,7 @@ export class NPC {
     if (!this.isShop || !this.shopItems) {
       return `${this.name} is not a shopkeeper.`;
     }
-    console.log(item);
+
     if (item.value) {
       player.gold += item.value;  // Player's gold is increased
       player.inventory.removeItem(item.name, 0);  // Removes the item from the player's inventory
